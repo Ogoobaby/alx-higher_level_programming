fa6311d09bac0eb7a406367f4ff094f8465b7502
Ogoobaby <ogochukwumarygloria16@gmail.com>
2023-01-30 11:49:06 -0800

    Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Ogochukwu GloriaMary Nwanochiri <alxsew.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
